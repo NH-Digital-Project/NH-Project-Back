888c0642c1d5f43a4edf6d725a8dc9cbb290cf46
yu-rim98 <rimmm098@gmail.com>
2026-05-13 17:39:46 +0900

    Feat: CustomException 정의

---
+++ src/main/java/com/example/demo/global/exception/ErrorCode.java
@@ -0,0 +1,21 @@
+package com.example.demo.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    // Auth
+
+    // User
+
+    // Common
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다."),
+    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
