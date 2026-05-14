98072e36fc05b4c883a2f66bd3787d97b037f20a
yu-rim98 <rimmm098@gmail.com>
2026-05-14 16:48:44 +0900

    Feat: User, Application Entity 어노테이션, 필드, private 생성자 추가

--- src/main/java/com/example/demo/domain/application/entity/Application.java
+++ src/main/java/com/example/demo/domain/application/entity/Application.java
@@ -1,18 +1,33 @@
 package com.example.demo.domain.application.entity;
 
+import com.example.demo.domain.user.entity.User;
 import com.example.demo.global.common.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "applications")
 @Entity
 public class Application extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", unique = true)
+    private User user;
 
     private String userName;
 
@@ -53,5 +68,33 @@ public class Application extends BaseEntity {
     @Column(columnDefinition = "TEXT")
     private String fundingPlan;
 
+    private Application(Long id, User user, String userName, String birthDate, String phoneNumber,
+        String applicationNumber, String farmName, String affiliatedNhName, String farmAddress,
+        String businessRegistrationNumber, String mainProduct, Integer annualSales,
+        Boolean onlineDistributionExperience, String productCategory, String shippingDate,
+        String fundingDesiredDate, String productName, String productSize, Integer sellingPrice,
+        Integer availableQuantity, String fundingPlan) {
+        this.id = id;
+        this.user = user;
+        this.userName = userName;
+        this.birthDate = birthDate;
+        this.phoneNumber = phoneNumber;
+        this.applicationNumber = applicationNumber;
+        this.farmName = farmName;
+        this.affiliatedNhName = affiliatedNhName;
+        this.farmAddress = farmAddress;
+        this.businessRegistrationNumber = businessRegistrationNumber;
+        this.mainProduct = mainProduct;
+        this.annualSales = annualSales;
+        this.onlineDistributionExperience = onlineDistributionExperience;
+        this.productCategory = productCategory;
+        this.shippingDate = shippingDate;
+        this.fundingDesiredDate = fundingDesiredDate;
+        this.productName = productName;
+        this.productSize = productSize;
+        this.sellingPrice = sellingPrice;
+        this.availableQuantity = availableQuantity;
+        this.fundingPlan = fundingPlan;
+    }
 }
 
--- src/main/java/com/example/demo/domain/user/entity/User.java
+++ src/main/java/com/example/demo/domain/user/entity/User.java
@@ -5,7 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
 @Entity
 public class User extends BaseEntity {
 
@@ -16,4 +23,14 @@ public class User extends BaseEntity {
     private String naverId;
 
     private String naverName;
+
+    private Boolean applied;
+
+
+    private User(Long id, String naverId, String naverName, Boolean applied) {
+        this.id = id;
+        this.naverId = naverId;
+        this.naverName = naverName;
+        this.applied = applied;
+    }
 }
