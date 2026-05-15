ea6dd89f5a9c9da442cf1807c22a1f26c764873a
yu-rim98 <rimmm098@gmail.com>
2026-05-15 13:37:14 +0900

    Build: Security 의존성 제거 (추후 추가 예정)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
-import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+//import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+//import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication( exclude = {
-    SecurityAutoConfiguration.class,
-    UserDetailsServiceAutoConfiguration.class
-})
+@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
