e53dac8f6080b9fb7442d56a003b777465aaf777
yu-rim98 <rimmm098@gmail.com>
2026-05-15 15:22:22 +0900

    Refactor: class -> record 형식으로 변경

--- src/main/java/com/example/demo/domain/user/dto/response/MyInfoResponse.java
+++ src/main/java/com/example/demo/domain/user/dto/response/MyInfoResponse.java
@@ -2,23 +2,17 @@ package com.example.demo.domain.user.dto.response;
 
 import com.example.demo.domain.user.entity.User;
 import java.time.LocalDateTime;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@Getter
-@RequiredArgsConstructor
-public class MyInfoResponse {
-
-    private final Long userId;
-
-    private final String naverId;
-
-    private final String naverName;
-
-    private final LocalDateTime createdAt;
+public record MyInfoResponse (
+    Long userId,
+    String naverId,
+    String naverName,
+    LocalDateTime createdAt
+) {
 
     public static MyInfoResponse from(User user) {
         return new MyInfoResponse(user.getId(), user.getNaverId(), user.getNaverName(),
             user.getCreatedAt());
     }
+
 }
