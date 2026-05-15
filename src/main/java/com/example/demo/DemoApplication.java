deee6fbe3a483f4e000dbcf690756cc6df596cac
yu-rim98 <rimmm098@gmail.com>
2026-05-15 14:01:23 +0900

    Refactor: 주석 제거

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
-//import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
 @SpringBootApplication
 public class DemoApplication {
