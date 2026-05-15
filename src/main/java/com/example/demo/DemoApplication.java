6db3d992edc2d37f9c735b4e6f54d9183bfc5c6b
fhdl1221 <fhdl353212@gmail.com>
2026-05-15 15:45:24 +0900

    Security 코드 주석 처리 및 삭제

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
-import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication( exclude = {
-    SecurityAutoConfiguration.class,
-    UserDetailsServiceAutoConfiguration.class
-})
+@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
