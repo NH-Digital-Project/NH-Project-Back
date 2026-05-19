841de74e58b6ced64b17007ce1dee90cf093f36f
yu-rim98 <rimmm098@gmail.com>
2026-05-19 16:22:26 +0900

    Feat: BEARER_PREFIX.length() 활용

--- src/main/java/com/example/demo/global/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/global/security/jwt/JwtAuthenticationFilter.java
@@ -39,7 +39,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private String resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
-            return bearerToken.substring(7);
+            return bearerToken.substring(BEARER_PREFIX.length());
         }
         return null;
     }
