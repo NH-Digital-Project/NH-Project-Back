4be2da2bf79ef6c115e85ac244aa82b167a96c5d
ghkdquddlf <xhdlapdlxm12@naver.com>
2026-05-19 14:51:31 +0900

    feat: 지원자 조회 DTO 작성

---
+++ src/main/java/com/example/demo/domain/admin/dto/response/ApplicationListResDto.java
@@ -0,0 +1,31 @@
+package com.example.demo.domain.admin.dto.response;
+
+import com.example.demo.domain.application.entity.Application;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+
+@Builder
+@Getter
+public class ApplicationListResDto {
+    private final int currentPage;
+    private final Long totalCount;
+    private final int totalPages;
+    private final List<ApplicationSummaryDto> applications;
+
+    public static ApplicationListResDto from(Page<Application> applications){
+        List<ApplicationSummaryDto> applicationDtos = applications.stream()
+                                                .map(ApplicationSummaryDto::from)
+                                                .toList();
+        return ApplicationListResDto.builder()
+                   .currentPage(applications.getNumber()+1)
+                   .totalCount(applications.getTotalElements())
+                   .totalPages(applications.getTotalPages())
+                   .applications(applicationDtos)
+                   .build();
+    }
+
+
+}
