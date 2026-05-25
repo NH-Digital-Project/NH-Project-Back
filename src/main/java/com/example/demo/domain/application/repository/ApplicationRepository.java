3e23d42d9dce0bd9943d78cf3b9815a6544bf9ab
fhdl1221 <fhdl353212@gmail.com>
2026-05-25 15:22:13 +0900

    refactor: 미사용 메서드 제거

--- src/main/java/com/example/demo/domain/application/repository/ApplicationRepository.java
+++ src/main/java/com/example/demo/domain/application/repository/ApplicationRepository.java
@@ -9,13 +9,9 @@ import java.util.Optional;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
 
-    boolean existsByUserId(Long userId);
-
     // 전달받은 날짜(today)로 시작하는 지원서 번호를 내림차순으로 정렬하여 가장 최근 번호 조회
     Optional<Application> findTopByApplicationNumberStartingWithOrderByApplicationNumberDesc(
         String today);
-
-    Optional<Application> findByUserId(Long userId);
 
     Page<Application> findByUserNameContainingOrFarmNameContaining(String userName, String farmName,
         Pageable pageable);
