1ba13420da5286984d63c637a893ed2452f8a197
yu-rim98 <rimmm098@gmail.com>
2026-05-28 18:55:12 +0900

    Refactor: 메서드 추출

--- src/main/java/com/example/demo/domain/user/service/UserService.java
+++ src/main/java/com/example/demo/domain/user/service/UserService.java
@@ -20,21 +20,24 @@ public class UserService {
 
     public MyInfoResponse getMyInfo(Long userId) {
       
-        User user = userRepository.findById(userId)
-            .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        User user = getOrElseThrow(userId);
 
         return MyInfoResponse.from(user);
     }
-  
-  
+
+
     @Transactional
     public void deleteMe(Long userId) {
 
-        User user = userRepository.findById(userId)
-            .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        User user = getOrElseThrow(userId);
 
         user.withdraw();
 
         // Todo 지원서 제출 상태에서도 탈퇴 가능 여부 확인
+    }
+
+    private User getOrElseThrow(Long userId) {
+        return userRepository.findById(userId)
+            .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
     }
 }
